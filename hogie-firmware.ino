b4c6085c99a3de890cb4d6e75a17e6ff7faf38ee
HunterH445318 <hendry.hunter04@gmail.com>
2023-03-07 18:40:47 -0500

    created minimum probably working topside code

--- hogie-firmware.ino
+++ hogie-firmware.ino
@@ -1,21 +1,21 @@
 
 #include <RH_ASK.h>
 // #include <Servo.h>  // Arduino's Servo library did not work, timer conflicts with RadioHead
-#include <Adafruit_SoftServo.h> // We used this servo instead
+#include <Adafruit_SoftServo.h>  // We used this servo instead
 #include <SPI.h>
 
-
+const int RX_CS_PIN = 9;
 
 // Servo myservo;
 Adafruit_SoftServo myservo;
 
-RH_ASK driver; // reciever CS=10 , reciever data = 11, transmitter data = 12 
+RH_ASK driver;  // reciever CS=10 , reciever data = 11, transmitter data = 12
 //pins
 int WATER_PIN = A0;
 int SERVO_PIN = 9;
 int IR_PIN = 2;
-const long int DRIVE_TIME = 1000;
-long int motor_Stop = 0;
+const uint64_t DRIVE_TIME = 1000;
+uint64_t motor_Stop = 0;
 
 //variables
 bool isWet = false;
@@ -24,9 +24,9 @@ int waterVal;                        //Analog value of water
 static byte servoDiveCommand = 0;    // angle value that the dive method sends to the servo
 static byte servoRiseCommand = 180;  // angle value that the rise method sends to the servo
 static byte servoCoastCommand = 90;
-long int time_elapsed = millis();  //
-long int start_time = 0;
-long int current_time = start_time + current_time;
+uint64_t unix_epoch_offset = 0;  //
+uint64_t start_time = 0;
+uint64_t current_time = start_time + current_time;
 
 
 void setup() {
@@ -38,6 +38,9 @@ void setup() {
 
   if (!driver.init())
     Serial.println("radio init failed");
+
+  pinMode(RX_CS_PIN, OUTPUT);
+  digitalWrite(RX_CS_PIN, HIGH);
 }
 
 void dive() {
@@ -95,25 +98,46 @@ void checkBottom(bool &isBottom) {
   }
 }
 
+
+void printull(uint64_t num) {
+  for (int i = 0; i < sizeof(num); i++) {
+    if ((uint8_t)((num >> (sizeof(num) - i - 1) * 8) & 0xFF) < 0x1F) {
+      Serial.print(0);
+    }
+    Serial.print((uint8_t)((num >> (sizeof(num) - i - 1) * 8) & 0xFF), HEX);
+  }
+}
+
 /**
  * recieve data from radio reciever , returns -1 if no data found
  */
-long int recieve() {
-  uint8_t buf[sizeof(long int)];
-  uint8_t buflen = sizeof(long int);
+uint64_t recieve() {
+  uint8_t buf[sizeof(uint64_t)] = { 0 };
+  uint8_t buflen = sizeof(uint64_t);
   // Non-blocking check
-  if (driver.recv(buf, &buflen)) {
-    return *((long *)buf);
-  }
-  return -1;
+  digitalWrite(RX_CS_PIN, HIGH);
+  while (!driver.recv(buf, &buflen)) {}
+  Serial.print("RX: 0x");
+  printull((uint64_t)(*((uint64_t *)buf)));
+  Serial.println();
+  return *((uint64_t *)buf);
 }
 
 /**
  * transmits time over radio transmitter 
  */
-void transmit(long time) {
-  driver.send((uint8_t *)time, sizeof(time));
+void transmit(uint64_t time) {
+  digitalWrite(RX_CS_PIN, LOW);
+  Serial.print("TX: 0x");
+  printull(time);
+  Serial.println();
+  driver.send((uint8_t *)&time, sizeof(time));
   driver.waitPacketSent();
+  digitalWrite(RX_CS_PIN, HIGH);
+}
+
+uint64_t trueTime() {
+  return unix_epoch_offset + (millis()) / 1000;
 }
 
 int state = 0;
@@ -122,53 +146,65 @@ void loop() {
   checkWet(isWet);
   checkBottom(isBottom);
   static bool endstop_switch;
-  Serial.println();
   Serial.print(state);
   Serial.print(",");
   Serial.print(isBottom);
   Serial.print(",");
   Serial.print(isWet);
   Serial.print(",");
-  Serial.print(motor_Stop);
-  Serial.print(",");
+  Serial.print((uint32_t)motor_Stop);
+  Serial.println();
 
+  uint64_t message_temp;
 
   switch (state) {
     case 0: /* Callibration State */
+      Serial.println("state 0");
 
-      int message = recieve();
+      message_temp = recieve();
 
-      while (message != 0xDEADBEEF) {
-        message = recieve();
+      while (message_temp != 0xDEADBEEF) {
+        message_temp = recieve();
+        delay(10);
       }
       state = 1;
-
       break;
     case 1: /* Wait State */
-      unsigned long timbuktu = millis() + 1000;
-      unsigned long timid;
-      transmit(timid);
-      while (millis() < timbuktu) {
-        timid = recieve();
-        if (timid == 010010) {
+      Serial.println("state 1");
+      transmit(trueTime());
+      message_temp = recieve();
+      if (message_temp != -1) {
+        if (message_temp == 0xFA110000) {
+          Serial.println("Got drop");
           state = 2;
           break;
+        } else {
+          unix_epoch_offset = message_temp - (millis() / 1000);
+          Serial.print("Calibrated offset: ");
+          printull(unix_epoch_offset);
+          Serial.print("; Calibrated offset gives: ");
+          printull(trueTime());
+          Serial.println();
         }
       }
-      transmit(current_time);
-
       break;
     case 2: /* falling state */
+      Serial.println("state 2");
       while (!isBottom) {
         dive();
       }
       state = 3;
       break;
     case 3:
+      Serial.println("state 3");
       while (isWet) {
         rise();
       }
       state = 1;
+      break;
+    default:
+      Serial.println("Reached default in state machine (very bad) :(");
+      while (1) {}
       break;
   }
 }
---
+++ hogie-topside/hogie-topside.ino
@@ -0,0 +1,123 @@
+#include <RH_ASK.h>
+RH_ASK driver;
+
+const int RX_CS_PIN = 9;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!driver.init())
+    Serial.println("radio init failed");
+
+  pinMode(3, INPUT_PULLUP);
+
+  pinMode(RX_CS_PIN, OUTPUT);
+  digitalWrite(RX_CS_PIN, HIGH);
+}
+
+void printull(uint64_t num) {
+  for (int i = 0; i < sizeof(num); i++) {
+    if ((uint8_t)((num >> (sizeof(num) - i - 1) * 8) & 0xFF) < 0x1F) {
+      Serial.print(0);
+    }
+    Serial.print((uint8_t)((num >> (sizeof(num) - i - 1) * 8) & 0xFF), HEX);
+  }
+}
+
+uint64_t getTime() {
+  Serial.println("time");
+  int rx = '\0', idx = 0;
+  char buf[20] = { '\0' };
+  while (rx != '\n' || idx >= 20) {
+    rx = Serial.read();
+    if (rx != -1) {
+      buf[idx++] = rx;
+    }
+  }
+  return (uint64_t)String(buf).toDouble();
+}
+
+uint64_t recieve() {
+  uint8_t buf[sizeof(uint64_t)];
+  uint8_t buflen = sizeof(uint64_t);
+  // Non-blocking check
+  if (driver.recv(buf, &buflen)) {
+    Serial.print("RX: 0x");
+    printull((uint64_t)(*((uint64_t *)buf)));
+    Serial.println();
+    return *((uint64_t *)buf);
+  }
+  return -1;
+}
+
+/**
+ * transmits time over radio transmitter 
+ */
+void transmit(uint64_t time) {
+  digitalWrite(RX_CS_PIN, LOW);
+  Serial.print("TX: 0x");
+  printull(time);
+  Serial.println();
+  driver.send((uint8_t *)&time, sizeof(time));
+  driver.waitPacketSent();
+  digitalWrite(RX_CS_PIN, HIGH);
+}
+bool Within(uint64_t num, uint64_t cmp, uint64_t distance) {
+  return (num < (cmp + distance)) && (num > (cmp - distance));
+}
+
+
+void loop() {
+  bool IsPressed = false;
+  while (digitalRead(3) == HIGH)
+    ;
+  Serial.println("Writing init message...");
+  while (digitalRead(3) == LOW) {
+    // Transmit while we wait for button release
+    transmit(0xDEADBEEF);
+  }
+  while (digitalRead(3) == HIGH)  // Wait for button press
+    ;
+  Serial.println("Calibrating time...");
+
+  uint64_t tx_time = 16776337352ull;  // About Feb 28 8:20 PM
+  long TimeOut = millis() + 250;
+  while (recieve() != tx_time) {
+    // Recieve data for longer (doing it once per tx failed to recieve any data)
+    TimeOut = millis() + 250;
+    while (recieve() != tx_time && millis() < TimeOut) {}
+    if (millis() < TimeOut) {
+      // Wre left because of a valid transmission, not timeout
+      break;
+    }
+    // Trainsmity periodically while we haven't confirmed the hoagie's time
+    tx_time = getTime();
+    transmit(tx_time);
+  }
+  Serial.println("Calibrated, press button to drop.");
+
+  while (digitalRead(3) == LOW) {
+    // Wait for button release
+  }
+
+  while (digitalRead(3) == HIGH) {
+    // Wait for button press
+  }
+
+  // When we get here, the hogie has properly calibrated it's time
+  while (true) {
+    Serial.println("Writing drop message");
+    transmit(0xFA110000);
+
+    while (digitalRead(3) == LOW) {
+      // Wait for button release
+    }
+
+    while (digitalRead(3) == HIGH) {
+      // Wait for hogie until button press
+      long rx_time = recieve();
+      if (rx_time != -1)
+        Serial.println(rx_time);
+    }
+  }
+}
