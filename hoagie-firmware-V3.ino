4a6f479052fcb6714b9534ece4f7b06b968d6bab
theNickHolbrook <nicholasjholbrook@gmail.com>
2025-02-05 18:47:08 -0500

    realized my cool feature wasnt as cool as I thought

--- hoagie-firmware-V3.ino
+++ hoagie-firmware-V3.ino
@@ -151,7 +151,7 @@ void loop() {
             break;
         case 4://waiting state
             pressure_data.append(packet(trueTime(), analogRead(PIN_PRESSURE_SENSOR)));
-            if (!(TIME_DIVE - ((millis() / 100) - Time_of_Dive))) state = 3;
+            if ((TIME_DIVE - ((millis() / 100) - Time_of_Dive)) < 0) state = 3;
             break;
         default:
             Serial.println("Error: Invalid state");
