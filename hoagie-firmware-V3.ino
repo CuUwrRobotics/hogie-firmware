50b6c4439b19f79f9f2a23abcabc682f8c04b3b3
ChesterMK7 <159496336+ChesterMK7@users.noreply.github.com>
2025-02-26 18:45:20 -0500

    updated transmitter code

--- hoagie-firmware-V3.ino
+++ hoagie-firmware-V3.ino
@@ -4,14 +4,30 @@
 #include "MS5837.h"
 #include "ArduPID.h"
 #include <Servo.h>
+#include <Vector.h>
+#include "printf.h"
+
+struct packet {
+    packet() {companyName = 69420; seconds = 0;
+        depth = 0.0; pressure = 0.0;}
+    packet(uint64_t s, float d, float p) {
+        companyName = 69420; seconds = s; depth = d; pressure = p;}
+    uint32_t companyName;
+    uint64_t seconds;
+    float depth, pressure;
+};
 
 /* Pin definitions */
 const int PIN_SERVO_LEFT = 3;
 const int PIN_PRESSURE_SENSOR = A0;
-const RF24 SENDER(7, 8);
-const RF24 RECEIVER(9, 10);
-const byte S_ADDRESS[6] = "00001";
-const byte R_ADDRESS[6] = "00002";
+#define CE_PIN 7
+#define CSN_PIN 8
+RF24 radio(CE_PIN, CSN_PIN);
+uint8_t address[][6] = { "1Node", "2Node" };
+
+/* Radio parameters */
+bool radioNumber = 1;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+//bool role = false;  // true = TX role, false = RX role
 
 /* Constants */
 const float PRESSURE_THRESHOLD = 100.0;
@@ -31,13 +47,7 @@ double PID_input, PID_output, setpoint = 2.5;
 const double kP = 0, kI = 0, kD = 0;
 uint64_t detectionStartTime = 0;
 float depth = 0.0;
-//Vector<uint32_t> pressure_data;
-packet pressure_data[] = [128];
-
-struct packet {
-    uint32_t time;
-    float pressure;
-};
+packet payload, temp;
 
 void setup() {
     Serial.begin(9600);
@@ -47,6 +57,11 @@ void setup() {
     pressure_sensor.setFluidDensity(997);
     PID.begin(&PID_input, &PID_output, &setpoint, kP, kI, kD);
     PID.setOutputLimits(-90, 90);
+    if (!radio.begin()) {Serial.println(F("radio hardware is not responding!!"));}
+    radio.setPALevel(RF24_PA_MAX);
+    radio.openWritingPipe(address[radioNumber]);
+    radio.openReadingPipe(1, address[!radioNumber]);
+    radio.startListening();
     Serial.println("Hoagie Firmware V2 Initialized");
 }
 
@@ -66,61 +81,54 @@ bool riseCompleted() {
     return getDepth() <= 0;
 }
 
-void transmit(String message)
-{
-  // Transmit Code
-  SENDER.begin();
-  SENDER.openWritingPipe(S_ADDRESS);
-  SENDER.setPALevel(RF24_PA_MIN);
-
-  const char text[] = message;
-  SENDER.write(&text, sizeof(text));
-  delay(1000);
-  sender.closeWritingPipe();
-}
-
-int64_t packetToBin(packet data[], TEAM_NAME)
-{
-  int64_t bin = 0;
-  bin |= (TEAM_NAME << 56);
-  bin |= (data[0].time << 48);
-  bin |= (data[0].pressure << 32);
-  bin |= (data[1].time << 16);
-  bin |= (data[1].pressure);
-  return bin;
-}
-
-
-String receive() {
-    RECEIVER.begin();
-    RECEIVER.openReadingPipe(0, R_ADDRESS);
-    RECEIVER.setPALevel(RF24_PA_MIN);
-    RECEIVER.startListening();
-    if (RECEIVER.available()) {
-        char text[32] = "";
-        RECEIVER.read(&text, sizeof(text));
-        return String(text);
+void transmit_packet(Vector<packet> d) {
+    int s = d.size();
+    radio.stopListening();
+    for (int i; i < d.size(); i++) {
+        radio.write(&d[i], sizeof(d[i]));
     }
-    return "";
+    radio.startListening();
+}
+
+void transmit_str(String s) {
+    radio.stopListening();
+    radio.write(&s, sizeof(s));
+    radio.startListening();
+}
+
+String receive_str() {
+    uint8_t pipe;
+    String payload;
+    if (radio.available(&pipe)) {              // is there a payload? get the pipe number that received it
+        uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
+        radio.read(&payload, bytes);
+        return payload;
+    }
+    else return "";
 }
 
 void loop() {
     static uint8_t state = 1;
     static uint64_t timer = millis();
-    static uint64_t message;
+    Vector<packet> diveData;
     switch (state) {
         case 1: // Wait State
             Serial.println("Waiting for signal");
-            transmit("Requesting time");
-            if (receive() == "START") {
+            transmit_str("Requesting time");
+            if (receive_str() == "START") {
                 state = 2;
                 PID.start();
             }
+            state = 2;
             break;
         case 2: // Diving
             Serial.println("Diving");
             if (millis() - timer >= 5000) {
-                pressure_data.append(packet(trueTime(), analogRead(PIN_PRESSURE_SENSOR)));
+                //pressure_data.push_back(getDepth());
+                temp.seconds = timer;
+                temp.depth = getDepth();
+                temp.pressure = pressure_sensor.pressure();
+                diveData.push_back(temp);
                 timer = millis();
             }
             PID_input = getDepth();
@@ -136,7 +144,11 @@ void loop() {
         case 3: // Rising
             Serial.println("Rising");
             if (millis() - timer >= 5000) {
-                pressure_data.append(packet(trueTime(), analogRead(PIN_PRESSURE_SENSOR)));
+                //pressure_data.push_back(getDepth());
+                temp.seconds = timer;
+                temp.depth = getDepth();
+                temp.pressure = pressure_sensor.pressure();
+                diveData.push_back(temp);
                 timer = millis();
             }
             PID_input = getDepth();
@@ -145,12 +157,13 @@ void loop() {
             if (riseCompleted()) {
                 state = 1;
                 PID.stop();
-                message = packetToBin(pressure_data, TEAM_NAME);
-                transmit(message);
+                //transmit("Data Sent");
+                transmit_packet(diveData);
+                diveData.clear();
             }
             break;
         case 4://waiting state
-            pressure_data.append(packet(trueTime(), analogRead(PIN_PRESSURE_SENSOR)));
+            //pressure_data.append(packet(trueTime(), analogRead(PIN_PRESSURE_SENSOR)));
             if ((TIME_DIVE - ((millis() / 100) - Time_of_Dive)) < 0) state = 3;
             break;
         default:
